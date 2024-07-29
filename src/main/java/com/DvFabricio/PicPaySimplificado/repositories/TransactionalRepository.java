4d693d447762a9971dfb7c5feed7532406314e2c
Fabricio <fa.engeroff1996@gmail.com>
2024-07-29 06:34:08 -0300

    create TransactionalRepository

---
+++ src/main/java/com/DvFabricio/PicPaySimplificado/repositories/TransactionalRepository.java
@@ -0,0 +1,9 @@
+package com.DvFabricio.PicPaySimplificado.repositories;
+
+import com.DvFabricio.PicPaySimplificado.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionalRepository extends JpaRepository<Transaction, Long> {
+}
